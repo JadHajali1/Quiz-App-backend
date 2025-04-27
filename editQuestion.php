fcfe3e5b262da46dfbd5f4f3b1352d382a8e1f8b
JadHajali1 <jadhajali773@gmail.com>
2025-04-28 00:02:09 +0300

    edit question backend

--- editQuestion.php
+++ editQuestion.php
@@ -1,13 +1,27 @@
+<?php
+include("./connect.php");
+
+if (!isset($_SESSION['admin_id'])){
+    echo json_encode(["message" => "admin not loggedIn!!"]);
+}
 
 
+$data = json_decode(file_get_contents('php//input'), true);
 
 
+$quiz_id = $data['quiz_id'];
+$question_text = $data['question_text'];
+$correct_answer = $data['correct_answer'];
 
 
-$checkQuiestion = $connect->("SELECT * FROM quissez WHERE id = ?");
+$checkQuiestion = $connect->prepare("SELECT quizzes.id FROM quissez INNER JOIN quizzes ON questions.quiz_id = quizzes.id Where questions.id =?");
 $checkQuiestion->execute([$quiz_id]);
-$quiz =  $checkQuiestion->fetch(PDO::FETCH_ASSOC);
+$questionRow =  $checkQuiestion->fetch(PDO::FETCH_ASSOC);
 
-if(!$quiz){
-    echo json_encode(["message" => "quiz not found!"]);
+
+$editQuestion = $connect->prepare("UPDATE questons SET question_text = ?, corrcet_answer = ? WHERE id =?");
+if($editQuestion->execute([$title, $description, $quiz_id])){
+    echo json_encode(["message" => "question updated"]);
+}else{
+    echo json_encode(["message" => "faild to update the question!"]);
 }
