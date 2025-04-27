ff71793da97bbfbf56262a27a3f1aa2b3f4c0556
JadHajali1 <jadhajali773@gmail.com>
2025-04-27 19:19:52 +0300

    create connection

--- connect.php
+++ connect.php
@@ -1,1 +1,14 @@
 <?php 
+
+try{
+    $host="localhost";
+    $port = "3306";
+    $dbname ="quizapp";
+    $user = "root";
+    $password = "1234558739"; //random password
+    
+    $connect = new PDO("mysql:host=$host;port=$port,dbname=$dbname,$user,$password");
+
+}catch(\Throwable $e){
+    echo $e;
+}
